6f656df5bc57cbafdfda5365cfee47df9929313b
asjir <pa.paradysz@gmail.com>
2022-09-16 15:39:51 +0200

    docstring update

--- src/mutating.jl
+++ src/mutating.jl
@@ -78,13 +78,12 @@ end
 """
 `db[oldmodel](update_kwargs...)` returns an updated model.\n
 `db[model]()` will not run the update query, so `db[model]` is a closure to return the up-to-date model when called.
-# Examples
-```jldoctest
+# Examples```jldoctest
 julia> firstperson = first(db[Person[]]); firstperson.year_of_birth
 1940
 julia> updated = db[firstperson](year_of_birth=1941); firstperson.year_of_birth, updated.year_of_birth
 (1940, 1941)
-"""
+```"""
 Base.getindex(db::DB, m::AbstractModel) = (; kwargs...) -> _update(db, m, NamedTuple(kwargs))
 
 
