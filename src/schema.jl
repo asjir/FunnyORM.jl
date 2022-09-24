d1d07b666869ec0d20a7e8568da0f276907b213d
asjir <pa.paradysz@gmail.com>
2022-09-24 09:13:42 +0100

    initial dialect support + patch notnulls

--- src/schema.jl
+++ src/schema.jl
@@ -1,21 +1,25 @@
+import FunSQL.SQLDialect
+
 process_sql(s::String) =
     let nothingorcapture(x, idx=2) = isnothing(x) ? nothing : x.captures[idx]
         tname(s) = match(r"(CREATE\s+TABLE\s+\"?)((\w)+)(\"|\s|\()", s) |> nothingorcapture
         pkey(s) =
             let v1 = nothingorcapture(match(r"(PRIMARY\s+KEY\s*\(\s*\[?)((\w)+)\)(\]|\s|$|,)", s))
-                v2 = nothingorcapture(match(r"(^\s*\[?\s*)(\w+)(.*PRIMARY\s+KEY)"m, s))
+                v2 = nothingorcapture(match(r"((^|\())\s*\[?\s*)(\w+)(.*PRIMARY\s+KEY)"m, s))
                 isnothing(v1) ? v2 : v1
             end
         fkeys(s) = Dict(map(x -> x.captures[2] => (x.captures[4], x.captures[6]),
-            eachmatch(r"(FOREIGN\s+KEY\s*\(\s*\[?) (\w+) (\]?\s*\)\s*REFERENCES\s+\"?) (\w+) (\"?\s*\(\s*\[?) (\w+) (\]?\s*\)\s*) (\s|$|,)"x, s)))
+            eachmatch(r"(FOREIGN\s+KEY\s*\(\s*\[?) (\w+) (\]?\s*\)\s*REFERENCES\s+\"?) (\w+) (\"?\s*\(\s*\[?) (\w+) (\]?\s*\)\s*) (\s|$|,|\))"x, s)))  # TODO: is xm here for linebreak not \s in last clause better?
         notnulls(s) = map(x -> x.captures[2],
-            eachmatch(r" (^\s*) (\w+) (.+NOT\s+NULL.*$)"xm, s))
+            eachmatch(r" ((^|,|\()\s*) (\w+) (.+NOT\s+NULL.*(,|$|\)))"xm, s))
 
         Symbol(tname(s)) => (pkey(s), fkeys(s), notnulls(s))
     end
 
 getpk(tname::Symbol, sqlmap::Dict{Symbol,Tuple}) = sqlmap[tname][1]
-get_sqls(connection) = [x.sql for x in DBInterface.execute(connection, "SELECT * FROM sqlite_schema") if !ismissing(x.sql)]  # TODO: handle sqlite
+
+get_sqls(connection::DBInterface.Connection, dialectname::Symbol=SQLDialect(typeof(conn)).name) =
+    [x.sql for x in DBInterface.execute(connection, "SELECT * FROM sqlite_schema") if !ismissing(x.sql)]
 sqlmap(conn) = Dict(map(process_sql, get_sqls(conn))...)
 
 
